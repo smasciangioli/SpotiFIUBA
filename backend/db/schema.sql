1f7ba407ae235eb08d401e57b8e8323050ed4996
Mariano <mendezmariano76@gmail.com>
2026-01-09 10:30:12 +0000

    Cree esquema de la base de datos

---
+++ backend/db/schema.sql
@@ -0,0 +1,31 @@
+CREATE TABLE usuarios (
+    id SERIAL PRIMARY KEY,
+    nombre_usuario VARCHAR(100) NOT NULL,
+    fecha_creacion DATE NOT NULL,
+    fecha_modificacion DATE NOT NULL 
+);
+
+CREATE TABLE canciones (
+    id SERIAL PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    duracion DECIMAL(10,2) NOT NULL,
+    artista VARCHAR(100) NOT NULL,
+    usuario_id INTEGER NOT NULL REFERENCES usuarios(id),
+    fecha_creacion DATE NOT NULL,
+    fecha_modificacion DATE NOT NULL
+);
+
+CREATE TABLE playlists (
+    id SERIAL PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    creador_id INTEGER NOT NULL REFERENCES usuarios(id),
+    fecha_creacion DATE NOT NULL,
+    fecha_modificacion DATE NOT NULL
+);
+
+-- Tabla que vincula la playlist con sus canciones
+CREATE TABLE playlist_canciones (
+    playlist_id INTEGER NOT NULL REFERENCES playlists(id),
+    cancion_id INTEGER NOT NULL REFERENCES canciones(id),
+    PRIMARY KEY (playlist_id,cancion_id)
+);
